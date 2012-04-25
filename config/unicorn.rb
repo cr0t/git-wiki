c83984a7a5aa1707bfa9ae6bcdd7b2f0fb395330
Sergey Kuznetsov <kuznecov.sg@gmail.com>
2012-04-25 21:08:18 +0400

    Updated unicorn.rb

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,88 +1,67 @@
-# Sample verbose configuration file for Unicorn (not Rack)
-#
-# This configuration file documents many features of Unicorn
-# that may not be needed for some applications. See
-# http://unicorn.bogomips.org/examples/unicorn.conf.minimal.rb
-# for a much simpler configuration file.
-#
-# See http://unicorn.bogomips.org/Unicorn/Configurator.html for complete
-# documentation.
-
 APP_PATH = "/var/www/com.summercode.wiki"
 
-# Use at least one worker per core if you're on a dedicated server,
-# more will usually help for _short_ waits on databases/caches.
+rails_env = ENV["RAILS_ENV"] || "development"
+
+# 2 workers and 1 master
 worker_processes 2
 
-# Help ensure your application will always spawn in the symlinked
-# "current" directory that Capistrano sets up.
 working_directory APP_PATH + "/current" # available in 0.94.0+
 
-# listen on both a Unix domain socket and a TCP port,
-# we use a shorter backlog for quicker failover when busy
+# Load rails+github.git into the master before forking workers
+# for super-fast worker spawn times
+preload_app true
+
+# Restart any workers that haven't responded in 60 seconds
+timeout 60
+
+# Listen on a Unix data socket
 listen "/tmp/.unicorn_wiki_sock", :backlog => 64
 
-# nuke workers after 30 seconds instead of 60 seconds (the default)
-timeout 30
-
-# feel free to point this anywhere accessible on the filesystem
 pid APP_PATH + "/shared/pids/unicorn.pid"
-
-# By default, the Unicorn logger will write to stderr.
-# Additionally, ome applications/frameworks log to stderr or stdout,
-# so prevent them from going to /dev/null when daemonized here:
 stderr_path APP_PATH + "/shared/log/unicorn.stderr.log"
 stdout_path APP_PATH + "/shared/log/unicorn.stdout.log"
 
-# combine REE with "preload_app true" for memory savings
-# http://rubyenterpriseedition.com/faq.html#adapt_apps_for_cow
-preload_app true
-GC.respond_to?(:copy_on_write_friendly=) and
+##
+# REE
+# http://www.rubyenterpriseedition.com/faq.html#adapt_apps_for_cow
+if GC.respond_to?(:copy_on_write_friendly=)
   GC.copy_on_write_friendly = true
+end
+
+before_exec do |server|
+ ENV['BUNDLE_GEMFILE'] = APP_PATH + "/current/Gemfile"
+end
 
 before_fork do |server, worker|
+  ##
+  # When sent a USR2, Unicorn will suffix its pidfile with .oldbin and
+  # immediately start loading up a new version of itself (loaded with a new
+  # version of our app). When this new Unicorn is completely loaded
+  # it will begin spawning workers. The first worker spawned will check to
+  # see if an .oldbin pidfile exists. If so, this means we've just booted up
+  # a new Unicorn and need to tell the old one that it can now die. To do so
+  # we send it a QUIT.
+  #
+  # Using this method we get 0 downtime deploys.
+
   # the following is highly recomended for Rails + "preload_app true"
   # as there's no need for the master process to hold a connection
-  #defined?(ActiveRecord::Base) and
-  #  ActiveRecord::Base.connection.disconnect!
+  defined?(ActiveRecord::Base) and ActiveRecord::Base.connection.disconnect!
 
-  # The following is only recommended for memory/DB-constrained
-  # installations.  It is not needed if your system can house
-  # twice as many worker_processes as you have configured.
-  #
-  # # This allows a new master process to incrementally
-  # # phase out the old master process with SIGTTOU to avoid a
-  # # thundering herd (especially in the "preload_app false" case)
-  # # when doing a transparent upgrade.  The last worker spawned
-  # # will then kill off the old master process with a SIGQUIT.
-  # old_pid = "#{server.config[:pid]}.oldbin"
-  # if old_pid != server.pid
-  #   begin
-  #     sig = (worker.nr + 1) >= server.worker_processes ? :QUIT : :TTOU
-  #     Process.kill(sig, File.read(old_pid).to_i)
-  #   rescue Errno::ENOENT, Errno::ESRCH
-  #   end
-  # end
-  #
-  # Throttle the master from forking too quickly by sleeping.  Due
-  # to the implementation of standard Unix signal handlers, this
-  # helps (but does not completely) prevent identical, repeated signals
-  # from being lost when the receiving process is busy.
-  # sleep 1
+  old_pid = "#{server.config[:pid]}.oldbin"
+  if File.exists?(old_pid) && server.pid != old_pid
+    begin
+      Process.kill(:QUIT, File.read(old_pid).to_i)
+    rescue
+      # someone else did our job for us
+    end
+  end
 end
 
 after_fork do |server, worker|
-  # per-process listener ports for debugging/admin/migrations
-  # addr = "127.0.0.1:#{9293 + worker.nr}"
-  # server.listen(addr, :tries => -1, :delay => 5, :tcp_nopush => true)
-
-  # the following is *required* for Rails + "preload_app true",
-  #defined?(ActiveRecord::Base) and
-  #  ActiveRecord::Base.establish_connection
-
-  # if preload_app is true, then you may also want to check and
-  # restart any other shared sockets/descriptors such as Memcached,
-  # and Redis.  TokyoCabinet file handles are safe to reuse
-  # between any number of forked children (assuming your kernel
-  # correctly implements pread()/pwrite() system calls)
+  ##
+  # Unicorn master loads the app then forks off workers - because of the way
+  # Unix forking works, we need to make sure we aren't using any of the parent's
+  # sockets, e.g. db connection
+  defined?(ActiveRecord::Base) and ActiveRecord::Base.establish_connection
 end
